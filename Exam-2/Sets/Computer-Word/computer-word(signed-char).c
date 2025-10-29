296ac034eff3d5a0f35f8395aae347e1d4333565
saiimonn <gementizasgg08@gmail.com>
2025-10-29 22:40:06 +0800

    refactor: cooked

--- Exam-2/Sets/Computer-Word/computer-word(signed-char).c
+++ Exam-2/Sets/Computer-Word/computer-word(signed-char).c
@@ -1,40 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
-typedef signed int Set;
+typedef signed char Set;
 
 void init(Set *);
 void insert(Set *, int);
 void delete(Set *, int);
+bool isMember(Set, int);
 void displayBits(Set);
 void displaySet(Set);
-bool isMember(Set, int);
-
 Set Union(Set, Set);
 Set Intersection(Set, Set);
 Set Difference(Set, Set);
 
 int main() {
     Set A, B, C;
-
-    printf("=== INITIALIZATION ===\n");
     init(&A);
     init(&B);
 
-    printf("A = ");
-    displayBits(A);
-    printf("B = ");
-    displayBits(B);
-
-    printf("\n=== INSERT ELEMENTS ===\n");
-    insert(&A, 1);
-    insert(&A, 3);
-    insert(&A, 5);
-
+    printf("=== INSERT ELEMENTS ===\n");
+    insert(&A, 0);
+    insert(&A, 7);  // sign bit (MSB)
     insert(&B, 3);
-    insert(&B, 4);
-    insert(&B, 6);
+    insert(&B, 7);  // sign bit too
 
     printf("A = ");
     displayBits(A);
@@ -46,44 +34,30 @@ int main() {
     printf("B = ");
     displaySet(B);
 
-    printf("\n=== MEMBERSHIP TEST ===\n");
-    printf("Is 3 in A? %s\n", isMember(A, 3) ? "Yes" : "No");
-    printf("Is 4 in A? %s\n", isMember(A, 4) ? "Yes" : "No");
-
-    printf("\n=== DELETE ELEMENT ===\n");
-    delete(&A, 3);
-    printf("A after deleting 3: ");
-    displayBits(A);
-    printf("A = ");
-    displaySet(A);
-
     printf("\n=== UNION ===\n");
     C = Union(A, B);
-    printf("A ∪ B = ");
-    displaySet(C);
     displayBits(C);
+    displaySet(C);
 
     printf("\n=== INTERSECTION ===\n");
     C = Intersection(A, B);
-    printf("A ∩ B = ");
-    displaySet(C);
     displayBits(C);
+    displaySet(C);
 
     printf("\n=== DIFFERENCE ===\n");
     C = Difference(A, B);
-    printf("A − B = ");
-    displaySet(C);
     displayBits(C);
+    displaySet(C);
 
     return 0;
 }
 
 void init(Set *S) {
-    *S = 0;
+    *S = '\0';
 }
 
 void insert(Set *S, int pos) {
-    *S |= (1 << pos);
+    *S |= 1 << pos;
 }
 
 void delete(Set *S, int pos) {
@@ -95,20 +69,18 @@ bool isMember(Set S, int pos) {
 }
 
 void displayBits(Set S) {
-    int mask = sizeof(S) * 8 - 1;
+    unsigned char mask = 1 << (sizeof(S) * 8 - 1);
 
-    for(; mask >= 0; mask--) {
-        printf("%d", S >> mask & 1);
-        if(mask % 8 == 0) printf(" ");
+    for(; mask > 0; mask >>= 1) {
+        printf("%d", (S & mask) ? 1 : 0);
     }
+    printf("\t(Decimal: %4d)\n", S);
 }
 
 void displaySet(Set S) {
-    int mask = sizeof(S) * 8 - 1;
-
     printf("{ ");
-    for(; mask >= 0; mask--) {
-        if(S >> mask & 1) printf("%d ", mask);
+    for(int i = 0; i < 8; i++) {
+        if(isMember(S, i)) printf("%d ", i);
     }
     printf("}\n");
 }
